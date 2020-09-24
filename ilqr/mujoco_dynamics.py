7f6b7532a3130ba61265511fe019755705fbbd60
Jim Wang <wangjim19@berkeley.edu>
2020-09-24 01:52:59 -0700

    added set/get_state, step, constrain functions to mujoco_dynamics class

--- ilqr/mujoco_dynamics.py
+++ ilqr/mujoco_dynamics.py
@@ -16,10 +16,10 @@ class MujocoDynamics:
 
         Args:
             model_xml_path: Path of XML file of model.
-            frame_skip: Number of substeps for each call to step.
+            frame_skip: Number of timesteps simulated for each call to step.
             constrain: Whether to constrain actions before passing into model.
                 NOTE: Only set constrain to False if model has no forced constraints.
-            bounds: [action space, 2] numpy array specifying min, max for each action.
+            bounds: [action space, 2] array specifying min, max for each action.
                 Defaults to model-defined constraints if constrain=True and bounds unspecified.
                 NOTE: make sure user-defined bounds are not wider than model constraints.
             x_eps: Epsilon used for finite differencing wrt state.
@@ -34,7 +34,6 @@ class MujocoDynamics:
         self._model = load_model_from_path(model_xml_path)
         self._frame_skip = frame_skip
         self.sim = MjSim(self._model, nsubsteps = self._frame_skip)
-        self._state_size = self.sim.data.qpos.shape[0] + self.sim.data.qvel.shape[0]
         self._action_size = self.sim.data.ctrl.shape[0]
         self.constrained = constrain
         self.bounds = None
@@ -50,7 +49,7 @@ class MujocoDynamics:
     @property
     def state_size(self):
         """State size."""
-        return self._state_size
+        return self.sim.model.nq + self.sim.model.nv
 
     @property
     def action_size(self):
@@ -61,3 +60,46 @@ class MujocoDynamics:
     def dt(self):
         """Time elapsed per step"""
         return self.model.opt.timestep * self.frame_skip
+
+    def set_state(self, state):
+        """Sets state of simulator
+
+        Args:
+            state: numpy state vector
+        """
+        self.sim.data.qpos[:] = state[:self.model.nq]
+        self.sim.data.qvel[:] = state[self.model.nq:]
+
+    def get_state(self, state):
+        """Gets state of simulator
+
+        Returns:
+            numpy state vector
+        """
+        return np.concatenate([self.sim.data.qpos, self.sim.data.qvel])
+
+    def step(self, action):
+        """Simulates one timestep
+
+        Args:
+            action: numpy action vector (unconstrained)
+        Returns:
+            next state vector
+        """
+        self.sim.data.ctrl[:] = self.constrain(action)
+        self.sim.step()
+        return self.get_state()
+
+    def constrain(self, action):
+        """Calculates control vector to be passed into model, constraining if necessary
+
+        Args:
+            action: numpy action vector (unconstrained)
+        Returns:
+            control vector for model (constrained if necessary)
+        """
+        if not self.constrained:
+            return action
+        diff = (self.bounds[:, 1] - self.bounds[:, 0]) / 2.0
+        mean = (self.bounds[:, 1] + self.bounds[:, 0]) / 2.0
+        return diff * np.tanh(action) + mean
