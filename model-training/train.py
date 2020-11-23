530a650855f8849e73d437c05f5e1f16163b9eca
Jim Wang <wangjim19@berkeley.edu>
2020-11-22 18:27:22 -0800

    added basic training script

---
+++ model-training/train.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader
+
+if torch.cuda.is_available():
+    print("Using CUDA")
+    device = 'cuda'
+else:
+    print("Using CPU")
+    device = 'cpu'
+
+#define model
+class Model(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(Model, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(state_size + action_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, state_size)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+#retrieve data
+state_size = 4
+action_size = 1
+
+with open('data-collection/data/cartpole/observations.txt', 'r') as f:
+    observations = np.loadtxt(f)
+with open('data-collection/data/cartpole/actions.txt', 'r') as f:
+    actions = np.loadtxt(f)
+with open('data-collection/data/cartpole/next_observations.txt', 'r') as f:
+    next_observations = np.loadtxt(f)
+
+inputs = torch.from_numpy(np.hstack((observations.view().reshape((-1, state_size)), actions.view().reshape((-1, action_size))))).float()
+labels = torch.from_numpy(next_observations.view().reshape(-1, state_size)).float()
+train_data = TensorDataset(inputs, labels)
+print("size of dataset:", len(train_data))
+
+#define training parameters
+lr = 0.001
+n_epochs = 20
+batch_size = 20
+
+model = Model(state_size, action_size).to(device)
+loss_fn = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=lr)
+train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+
+#train
+def train_step(x, y):
+    model.train() #set model to train mode
+
+    optimizer.zero_grad()
+    yhat = model(x)
+    loss = loss_fn(y, yhat)
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+
+for epoch in range(n_epochs):
+    losses = []
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        loss = train_step(x_batch, y_batch)
+        losses.append(loss)
+    print("epoch", epoch, ": average loss =", loss)
